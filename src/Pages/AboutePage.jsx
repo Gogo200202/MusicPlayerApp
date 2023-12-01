cd174a6d2a41af64a5d276fc5ddce559c1847757
Gogo200202 <gosheto420@gmail.com>
2023-12-01 21:47:43 +0200

    add aboute page

---
+++ src/Pages/AboutePage.jsx
@@ -0,0 +1,14 @@
+function AboutePage() {
+  return (
+    <>
+      <div class="flex justify-center text-center text-5xl text-slate-50 mx-20 mt-6 mb-2">
+        Aboute pages
+      </div>
+      <div class="flex justify-center text-2xl items-center mx-20 text-slate-50">
+        This is just demostrration for my front end skil this site is not functional.
+      </div>
+    </>
+  );
+}
+
+export default AboutePage;
