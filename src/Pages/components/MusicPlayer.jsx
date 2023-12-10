97e0e52ad1829d4169bee57aa70a62564a353569
Gogo200202 <gosheto420@gmail.com>
2023-12-10 16:35:36 +0200

    Make play and stop button change

--- src/Pages/components/MusicPlayer.jsx
+++ src/Pages/components/MusicPlayer.jsx
@@ -1,84 +1,84 @@
-import React, { useEffect,useState, useCallback, memo, useRef } from "react";
+import React, { useEffect, useState, useCallback, memo, useRef } from "react";
 
+function MusicPlayer({ prop }) {
+  let [timeUpdate, setTimeUpdate] = useState();
 
- function MusicPlayer({prop}){
+  const start = () => {
+    prop.audio.play();
+    setButton(
+      <button className="mx-2" onClick={stop}>
+        Stop
+      </button>
+    );
+  };
 
-  let [timeUpdate,setTimeUpdate]=useState();
+  const stop = () => {
+    prop.audio.pause();
+    setButton(
+      <button className="mx-2" onClick={start}>
+        Play
+      </button>
+    );
+  };
 
-     const start = () => {
-        prop.audio.play();
-      };
-    
-      
-      const stop = () => {
-        prop.audio.pause();
-      };
+  const soundVolume = (e) => {
+    let num = e.target.value / 100;
+    prop.audio.volume = num.toFixed(2);
+  };
 
-      const soundVolume = (e) => {
-        let num = e.target.value / 100;
-        prop.audio.volume = num.toFixed(2);
-      };
+  const soundTime = (e) => {
+    let percentage = e.target.value / 100;
+    let result = prop.audio.duration * percentage;
+    prop.audio.currentTime = result;
+  };
 
-      const soundTime = (e) => {
-        let percentage = e.target.value / 100;
-        let result = prop.audio.duration * percentage;
-        prop.audio.currentTime = result;
-      };
-    
-      function ChangeTimeOnRage(e){
-        let result=(e.originalTarget.currentTime/e.originalTarget.duration)*100;
-        setTimeUpdate(result);
-    
-      }
-    
-      useEffect(() => {
-        prop.audio.addEventListener("timeupdate", ChangeTimeOnRage);
-      }, []);
+  function ChangeTimeOnRage(e) {
+    let result =
+      (e.originalTarget.currentTime / e.originalTarget.duration) * 100;
+    setTimeUpdate(result);
+  }
 
-    return (
-     
-          <div className=" text-center w-40 bg-gray-400 rounded-lg">
-            <button className="mx-2" onClick={start}>
-              
-              Play
-            </button>
-            <button className="mx-2" onClick={stop}>
-              
-              stop
-            </button>
-    
-    
-            <label
-              htmlFor="default-range"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              soundVolume
-            </label>
-            <input
-              id="default-range"
-              type="range"
-              className="ml-16 -rotate-90 w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-              onChange={soundVolume}
-            ></input>
-    
-            <label
-              htmlFor="default-range"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              video time
-            </label>
-            <input
-              id="default-range"
-              type="range"
-              className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-              value={timeUpdate|| '0'}
-              onChange={soundTime}
-            ></input>
-            <audio />
-          </div>
-      
-      );
+  useEffect(() => {
+    prop.audio.addEventListener("timeupdate", ChangeTimeOnRage);
+  }, []);
+  let [button, setButton] = useState(
+    <button className="mx-2" onClick={start}>
+      Play
+    </button>
+  );
 
+  return (
+    <div className=" text-center w-40 bg-gray-400 rounded-lg">
+      {button}
 
+      <label
+        htmlFor="default-range"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        soundVolume
+      </label>
+      <input
+        id="default-range"
+        type="range"
+        className="ml-16 -rotate-90 w-36 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        onChange={soundVolume}
+      ></input>
+
+      <label
+        htmlFor="default-range"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        video time
+      </label>
+      <input
+        id="default-range"
+        type="range"
+        className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        value={timeUpdate || "0"}
+        onChange={soundTime}
+      ></input>
+      <audio />
+    </div>
+  );
 }
 export default MusicPlayer;
